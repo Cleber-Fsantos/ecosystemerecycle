4a58f7d244c5e7484373164c91c76a36c747ca10
Cleberton F. Santos <cleber.cfsantos@gmail.com>
2023-06-07 20:44:35 -0300

    Responsivdade 1.0 da pagina nossa história

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//Criano váriavel em JS
+let menu = document.getElementById("menu_links")
+let menu_barras = document.getElementById("menu_barras")
+
+function mostrarMenu(){
+    if (window.getComputedStyle(menu).display == 'none'){
+        //Mostre o menu
+        menu.style.display = "flex";
+        menu_barras.setAttribute("aria-expanded", "true");
+        menu_barras.setAttribute("aria-label", "Fechar o Menu");
+
+    }else{
+        //Esconde o menu
+        menu.style.display = "none";
+
+        menu_barras.setAttribute("aria-expanded", "false");
+        menu_barras.setAttribute("aria-label", "Abrir o Menu");
+        
+    }
+};
+
+
+// function addEventListener("resize", () => {
+//     if (window.innerWidth > 1009){
+//         // menu.style.display = "flex";
+//         alert("Eu sou um alert!");
+//     } else{
+//         // menu.style.display = "none";
+//     }
+// });
